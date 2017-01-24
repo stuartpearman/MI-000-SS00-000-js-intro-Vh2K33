dbab93eeb6393dbe5a0e88207a9e3d83075a0a12
Stuart Pearman <swpearman@gmail.com>
2017-01-23 20:06:13 -0500

    put together the drum kit with js

---
+++ script.js
@@ -0,0 +1,27 @@
+// sounds
+
+var blip = new Audio('/sounds/blip.wav')
+var coin = new Audio('/sounds/coin.wav')
+var zap = new Audio('/sounds/zap.wav')
+var hurt = new Audio('/sounds/hurt.wav')
+
+// buttons
+
+var blipButton = document.getElementById('blipButton')
+var coinButton = document.getElementById('coinButton')
+var zapButton = document.getElementById('zapButton')
+var hurtButton = document.getElementById('hurtButton')
+
+function initSound(sound, soundButton) {
+	soundButton.addEventListener('mouseenter', function () {
+		sound.play()
+	})
+	soundButton.addEventListener('click', function () {
+		sound.play()
+	})
+}
+
+initSound(blip, blipButton)
+initSound(coin, coinButton)
+initSound(zap, zapButton)
+initSound(hurt, hurtButton)
